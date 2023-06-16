3ad40ea7ee8a344313922405505f9d585c50aaf7
manuka117 <88744615+manuka117@users.noreply.github.com>
2023-06-16 14:04:00 -0600

    Now stops the sound when another is requested

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import discord
 import env
-import asyncio
 
 from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio
 from discord.ui import Button, View
 
-
+# Download - https://ffmpeg.org/download.html
 ffmpeg_path = 'C:\ffmpeg\bin\ffmpeg.exe'
 discord.FFmpegPCMAudio.executable = ffmpeg_path
 
@@ -26,11 +25,11 @@ buttons_data = env.buttons_data
 
 @bot.event
 async def on_message(message):
-    # Ignora los mensajes del bot
+    # Ignore bot messages
     if message.author == bot.user:
         return
 
-    # Borra el mensaje anterior del bot
+    # Delete the previous message from the bot
     async for msg in message.channel.history(before=message):
         if msg.author == bot.user:
             await msg.delete()
@@ -48,8 +47,7 @@ async def on_message(message):
 
 @bot.event
 async def on_ready():
-    print(f'Conectado como {bot.user.name}')
-
+    print(f'Connected as {bot.user.name}')
 
 @bot.command()
 async def menu(ctx):
@@ -63,16 +61,15 @@ async def menu(ctx):
             button = Button(label=button_data["label"], style=button_data["style"])
             
         async def button_callback(interaction, sound=button_data["sound"]):
-            await sonido(ctx, sound)
+            await play_sound(ctx, sound)
             await interaction.response.defer()
 
         button.callback = button_callback
         view.add_item(button)
     await ctx.send(view=view)
 
-
 @bot.command()
-async def sonido(ctx, sonido):
+async def play_sound(ctx, sound):
     global voice_client
     
     if ctx.author.voice:
@@ -83,11 +80,13 @@ async def sonido(ctx, sonido):
         return
 
     if voice_client and voice_client.is_connected():
+        if voice_client.is_playing():
+            voice_client.stop()
         await voice_client.move_to(voice_channel)
     else:
         voice_client = await voice_channel.connect()
 
-    audio_source = FFmpegPCMAudio(sonido)
+    audio_source = FFmpegPCMAudio(sound)
     voice_client.play(audio_source)
 
 bot.run(token)
